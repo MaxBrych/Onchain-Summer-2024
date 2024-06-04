b387984a2281f6f7f8fcae02c318a594ce8e3015
Till is Bene <37979079+TilisBene@users.noreply.github.com>
2024-06-04 17:46:59 +0200

    added remix icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import { useRouter } from "next/navigation";
+import 'remixicon/fonts/remixicon.css';
 
 const client = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
@@ -37,20 +38,6 @@ export default function Home() {
             onChange={(e) => setSearchAddress(e.target.value)}
             className="w-full p-4 pl-12 mb-4 border rounded-full border-gray-400 justify-center align-middle text-black"
           />
-          <svg
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-4.35-4.35m1.45-6.4a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
-            ></path>
-          </svg>
         </form>
       </div>
     </main>
