4801bf2e7222ef2d5f929a8fdfda25b2f713f858
Till is Bene <37979079+TilisBene@users.noreply.github.com>
2024-06-04 18:00:12 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import { useRouter } from "next/navigation";
+import { RiWallet3Fill } from "@remixicon/react";
 
 const client = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
@@ -30,6 +31,7 @@ export default function Home() {
           Explore your onchain identity and style it
         </h1>
         <form onSubmit={handleSearchSubmit} className="flex items-center w-full max-w-md relative">
+        <RiWallet3Fill size={36} color="currentColor" className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
           <input
             type="text"
             placeholder="Enter wallet address"
@@ -37,20 +39,6 @@ export default function Home() {
             onChange={(e) => setSearchAddress(e.target.value)}
             className="w-full p-4 pl-12 mb-4 border rounded-full border-gray-400 justify-center align-middle text-black"
           />
-          <svg
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-4.35-4.35m1.45-6.4a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
-            ></path>
-          </svg>
         </form>
       </div>
     </main>
