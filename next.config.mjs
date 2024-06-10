4302467b04061ddbd172d1a06301987bc6ec7423
MaxBrych <max.brych03@gmail.com>
2024-06-10 18:23:32 +0200

    fix next.cofig error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[name][ext]",
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
